98d805da1d7037b8445192ea7f71164828be4f5a
STVOY66 <ethan.pauly@gmail.com>
2024-08-27 12:17:06 -0400

    Finished refactoring p1, added option for part 2 output

--- 2023/first-cpp.cpp
+++ 2023/first-cpp.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 #include <fstream>
+#include <cstdint>
+#define pINT(i) (i - '0')
 
 int part1(std::ifstream*);
+int part2(std::ifstream*);
 
-int main() {
+int main(int argc, char *argv[]) {
     std::ifstream input("./input-1.txt");
+    int ans;
+
+    if(pINT(*argv[1]) - 1)
+        ans = part1(&input);
+    else
+        ans = part2(&input);
     
-    std::cout << part1(&input) << "\n";
+    std::cout << "Answer to part " << argv[1] << "is: " << ans << "\n";
 
     return 0;
 }
 
 int part1(std::ifstream *input) {
     std::string ins;
-    std::string outs = "";
-    int outi;
+    uint8_t a, b, inlength;
     int sum = 0;
     
     while(*input >> ins) {
-        for(char& c : ins) {
-            if(isdigit(c)) outs += c;
-        }
-        outi = atoi((std::string(1, outs[0]) + outs[outs.length() - 1]).c_str());
-        sum += outi;
-        outs = "";
+        inlength = ins.length(); 
+
+        for(a = 0; !isdigit(ins[a]) && a < inlength; a++);
+        for(b = inlength - 1; !isdigit(ins[b]) && b >= 0; b--);
+
+        sum += pINT(ins[a])*10 + pINT(ins[b]);
     }
 
     return sum;
