ea1ef8b15a1560ec346584eebb601b95722f940f
STVOY66 <ethan.pauly@gmail.com>
2024-08-29 11:35:06 -0400

    Wrote function to turn word to int given const char*

--- 2023/first-cpp.cpp
+++ 2023/first-cpp.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <cstring>
 #define pINT(i) (i - '0')
 
 int part1(std::ifstream*);
 int part2(std::ifstream*);
 void findFLInt(std::string, int*);
 void findFLStr(std::string, int*);
+int parseInt(const char*);
 
 int main(int argc, char *argv[]) {
     std::ifstream input("./input-1.txt");
 
-    std::cout << "Answer to part 1 is: " << part1(&input) << "\n";
-    std::cout << "Answer to part 2 is: " << part2(&input) << "\n";
-
+    // std::cout << "Answer to part 1 is: " << part1(&input) << "\n";
+    // std::cout << "Answer to part 2 is: " << part2(&input) << "\n";
+    std::cout << parseInt("fivetynine") << "\n";
     return 0;
 }
 
@@ -36,6 +38,15 @@ int part2(std::ifstream *input) {
     return -1;
 }
 
+int parseInt(const char* input) {
+    std::string nums[] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    for(int i = 0; i <= 9; i++) {
+        if(strncmp(input, nums[i].c_str(), nums[i].length()) == 0) return i;
+    }
+
+    return -1;
+}
+
 void findFLInt(std::string input, int* out) {
     int a, b, slen;
     slen = input.length();
@@ -45,4 +56,8 @@ void findFLInt(std::string input, int* out) {
 
     out[0] = a;
     out[1] = b;
+}
+
+void findFLStr(std::string input, int* out) {
+    
 }
